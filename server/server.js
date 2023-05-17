e9a987292926e446546d9bffa95f4df3f3099fb4
Samantha Gosselin <samanthag2009@gmail.com>
2023-05-16 19:02:31 -0500

    began server.js, required packages and set up for routes, coded server for apollo and typeDefs and resolvers. Added TODOs around middleware and routes. NEED: AUTH MIDDLEWARE.

---
+++ server/server.js
@@ -0,0 +1,33 @@
+//requiring vital packages
+const express = require('express');
+const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+const {typeDefs, resolvers } = require('./schemas');
+//PLACEHOLDER - DOES NOT EXIST YET
+//const {authMiddleware } = require('./utils/auth');
+
+//instance of express and port with port number 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//TODO- require routes
+
+//init Apollo server
+
+const server = new ApolloServer ({
+    typeDefs,
+    resolvers
+});
+
+//TODO - ADD APOLLO MIDDLEWARE TO EXPRESS APPLICATION WITH UTILS AND AUTHS
+//server.applyMiddleWare({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//confirmation messages
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+  
