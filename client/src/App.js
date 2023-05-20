18f99f14bc6d1df3ebdf0e42c986894e74fc5fdc
Danny Boushka <boushka9@gmail.com>
2023-05-20 14:55:02 -0600

    Fixed slight styling issues that popped up w change to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from '././assets/logo-1.png'
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import NavBar from './components/NavBar';
-// import ToDo from './pages/ToDo'
+//import ToDo from './pages/ToDo'
 import Footer from './components/Footer'
 
 import React,{ useState } from 'react';
@@ -13,45 +13,48 @@ import './App.css';
 function App() {
   const [currentForm, setForm] = useState('login');
 
-  const toggleForm = (formName) => {
-    setForm(formName);
-  }
-  
+const toggleForm = (formName) => {
+setForm(formName);
+}
   return (
-    <div >
-      <div className="App">
-        {/* <ApolloProvider client={client}> */}
-          {/* <Router> */}
-            <header>
-              <img className='logo' src={logo} alt='Logo'/>
-              <NavBar />
-            </header> 
-            {/* <Routes>  */}
-                {/* <Route 
-                  path="/login" 
-                  element={<Login />} 
-                />
-                <Route 
-                  path="/signup" 
-                  element={<SignUp />} 
-                />
-                <Route 
-                  path="/todos" 
-                  element={<ToDo />} 
-                /> */}
-            {/* <Route
-                  path="/scores" 
-                  element={<Scores />} 
-                /> */}
-            {/* </Routes>
-          </Router> */}
-          <div className='login-sign'>
-             { 
-               currentForm === 'Login' ? <Login onFormSwitch={toggleForm}/> : <SignUp onFormSwitch={toggleForm}/>
-             }
+    <div className="App">
+      {/* <ApolloProvider client={client}> */}
+        {/* <Router>
+          <header>
+            <img className='logo' src={logo} alt='Logo'/>
+            <NavBar />
+          </header> 
+          <Routes> 
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<SignUp />} 
+              />
+              <Route 
+                path="/todos" 
+                element={<ToDo />} 
+              />
+              <Route
+                path="/scores" 
+                element={<Scores />} 
+              />
+          </Routes>
+      </Router> */}
 
-          </div> 
-        </div>   
+    {/*  COMMENTED OUT ROUTES TO WORK ON STYLING, COMMENT THEM BACK IN AND DELETE THE HEADER BELOW BC ONE IS ALREADY IN THE ROUTES  */}
+        <header>
+          <img className='logo' src={logo} alt='Logo'/>
+          <NavBar />
+        </header> 
+
+        <div className='login-sign'>
+           { 
+             currentForm === 'Login' ? <Login onFormSwitch={toggleForm}/> : <SignUp onFormSwitch={toggleForm}/>
+           }
+        </div> 
       <Footer />
      {/* </ApolloProvider> */}
     </div>
