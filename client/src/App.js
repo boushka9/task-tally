d1ee17712799d700118fbe0a50c39ad294269474
AshSox <119371427+AshSox@users.noreply.github.com>
2023-05-19 22:49:07 -0500

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import logo from '././assets/logo-1.png'
 import { Login } from './pages/Login';
 import { SignUp } from './pages/SignUp';
+import NavBar from './components/NavBar'
 
 import React,{ useState } from 'react';
 import './App.css';
@@ -12,12 +15,40 @@ setForm(formName);
 }
   return (
     <div className="App">
-     { 
-     currentForm === 'Login' ? <Login onFormSwitch={toggleForm}/> : <SignUp onFormSwitch={toggleForm}/>
-     }
+      <ApolloProvider client={client}>
+        <Router>
+      <header>
+        <img className='logo' src={logo} alt='Logo'/>
+        <NavBar />
+      </header> 
+      <Routes>
+               
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+                            <Route 
+                path="/signup" 
+                element={<SignUp />} 
+              />
+                            <Route 
+                path="/todos" 
+                element={<ToDo />} 
+              />
+                            <Route
+                path="/scores" 
+                element={<Scores />} 
+              />
+            </Routes>
+      </Router>
+     <div className='login-sign'>
+        { 
+          currentForm === 'Login' ? <Login onFormSwitch={toggleForm}/> : <SignUp onFormSwitch={toggleForm}/>
+        }
+     </div> 
+     </ApolloProvider>
     </div>
   );
 }
 
 export default App;
-
