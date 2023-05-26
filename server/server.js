32c151b490abc530fa87de203567662653b9536c
Danny Boushka <boushka9@gmail.com>
2023-05-26 12:27:21 -0600

    new update

--- server/server.js
+++ server/server.js
@@ -28,6 +28,9 @@ app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 const startApolloServer = async () => {
     await server.start();
